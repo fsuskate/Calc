3b62e911dcb37bcf413ddff7b06cc7604ce4da78
Francisco Surroca <fsurroca@francisco-surrocas-macbook-pro.local>
2013-11-14 20:33:10 -0500

    Initial Commit

---
+++ Calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calc
+//
+//  Created by Francisco Surroca on 11/14/13.
+//  Copyright (c) 2013 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calc
+//
+//  Created by Francisco Surroca on 11/14/13.
+//  Copyright (c) 2013 Francisco Surroca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
